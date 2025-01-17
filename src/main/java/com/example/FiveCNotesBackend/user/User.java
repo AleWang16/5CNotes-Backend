17db343cc0e1ab3e2dc8ec9e35a0baeb72a63864
Alejandro Wang <awang21913@gmail.com>
2025-01-17 00:30:25 -0500

    added some getters for user

--- src/main/java/com/example/FiveCNotesBackend/user/User.java
+++ src/main/java/com/example/FiveCNotesBackend/user/User.java
@@ -1,4 +1,28 @@
 package com.example.FiveCNotesBackend.user;
 
 public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
 }
