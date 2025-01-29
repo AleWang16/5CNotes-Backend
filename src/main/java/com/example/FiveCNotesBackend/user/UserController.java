093ef764c38473f55869e088da624f89cf1a8782
Alejandro Wang <awang21913@gmail.com>
2025-01-29 03:19:30 -0500

    wrote logic for userController

--- src/main/java/com/example/FiveCNotesBackend/user/UserController.java
+++ src/main/java/com/example/FiveCNotesBackend/user/UserController.java
@@ -1,4 +1,31 @@
 package com.example.FiveCNotesBackend.user;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "api/user")
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {this.userService = userService;}
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @PostMapping
+    public void addNewUser(@RequestBody User user) {
+        userService.addNewUser(user);
+    }
+
+    @DeleteMapping(path = "{userId}")
+    public void deleteStudent(@PathVariable("userId") UUID userId) {
+        userService.deleteUser(userId);
+    }
+
 }
